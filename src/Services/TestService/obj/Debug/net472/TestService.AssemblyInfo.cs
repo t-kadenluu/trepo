ad73040ae9f5764698066d6665308c25992e7ee4
MigrationBot <migrationbot@automated.com>
2025-07-30 16:09:20 -0700

    Migration completed - all changes applied

--- src/Services/TestService/obj/Debug/net472/TestService.AssemblyInfo.cs
+++ src/Services/TestService/obj/Debug/net472/TestService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b1c14667fd41d42d7dfd5fcc608dac6a63695c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91ab97da440d10708689834d804341b8e50313b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
