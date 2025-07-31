2772394d1d9cc33fd3eb53697cf5b0630db5a935
MigrationBot <migrationbot@automated.com>
2025-07-31 11:49:47 -0700

    Migration completed - all changes applied

--- migration-changelog.md
+++ migration-changelog.md
@@ -1,6 +1,6 @@
 # Migration Changelog
 
-Generated on: 2025-07-31 11:40:31
+Generated on: 2025-07-31 11:49:24
 
 ## Summary
 This file tracks all changes made during the migration process.
--- src/Services/TestService/obj/Debug/net472/TestService.AssemblyInfo.cs
+++ src/Services/TestService/obj/Debug/net472/TestService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19fdce10c874f6fa79b62e45ef5b99ab52526155")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c81da11aecead4914367099fc3f8723dd9f6901")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
